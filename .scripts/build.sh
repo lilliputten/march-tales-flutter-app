3ada9c6ca4c9f00728e65d1fed9e9b91df7a5074
lilliputten <lilliputten@gmail.com>
2026-05-15 03:48:39 +0300

    Updated flutter/android/gradle tooling to avoid a Google Play error "Your app does not support 16 KB memory page sizes".
    Error reference (https://developer.android.com/guide/practices/page-sizes):
    
    ```
    16 KB Google Play compatibility requirement
    
    Starting November 1st, 2025, all new apps and updates to existing apps submitted to Google Play and targeting Android 15+ devices must support 16 KB page sizes on 64-bit devices.
    
    ...
    ```

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # @desc Create apk/aab build
-# @changed 2025.04.18, 21:41
+# @changed 2026.05.15, 04:47
 
 if [ $# -lt 1 ]; then
   echo "Usage: $0 [--apk] [--aab]"
@@ -29,34 +29,36 @@ VERSION_CODE=`cat "$VERSION_CODE_PATH"`
 # TIMESTAMP=`date -r "$VERSION_PATH" "+%Y.%m.%d %H:%M:%S %z"`
 # TIMETAG=`date -r "$VERSION_PATH" "+%y%m%d-%H%M"`
 
-# Create apk build
-if [[ "$ARGS" =~ .*--apk.* ]]; then
-  APK_FOLDER="build/app/outputs/apk/release"
-  echo "Generating apk build $PROJECT_INFO #${VERSION_CODE}..."
-  flutter build apk \
-    --build-name=$VERSION \
-    --dart-define-from-file=.env \
-    && echo "See release in $APK_FOLDER" \
-    && ls -lah $APK_FOLDER/*${VERSION}*.apk \
-    && echo "OK"
-fi
-
-# Create signed bundle (aab)
+# Create a signed bundle (aab)
 if [[ "$ARGS" =~ .*--aab.* ]]; then
   AAB_FOLDER="build/app/outputs/bundle/release"
-  echo "Generating aab bundle $PROJECT_INFO #${VERSION_CODE}..."
-  DEBUG_INFO_PATH="build/app/intermediates/merged_native_libs/release/out/lib"
+  echo "Generating an aab bundle $PROJECT_INFO #${VERSION_CODE}..."
+  # Old debug info path (gradle 8.3):
+  # DEBUG_INFO_PATH="build/app/intermediates/merged_native_libs/release/out/lib"
+  # New debug info path (gradle 8.7):
+  DEBUG_INFO_PATH="build/app/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib"
   DEBUG_INFO_ARC="debug-symbols-${VERSION_CODE}-${VERSION}.zip"
   flutter build appbundle \
     --build-name=$VERSION \
     --dart-define-from-file=.env \
     --obfuscate \
     --split-debug-info="debug-info/"
-  echo "Creating the debug symbols archive..."
+  echo "Creating a debug symbols archive..."
   cd "$DEBUG_INFO_PATH" && zip -r "$DEBUG_INFO_ARC" * ; cd -
   mv "$DEBUG_INFO_PATH/$DEBUG_INFO_ARC" "$AAB_FOLDER/"
-  echo "See release in $AAB_FOLDER"
+  echo "See the aab bundle in the $AAB_FOLDER"
   cd $AAB_FOLDER && ls -lah *${VERSION}* ; cd -
   echo "OK"
 fi
 
+# Create an apk build
+if [[ "$ARGS" =~ .*--apk.* ]]; then
+  APK_FOLDER="build/app/outputs/apk/release"
+  echo "Generating an apk build $PROJECT_INFO #${VERSION_CODE}..."
+  flutter build apk \
+    --build-name=$VERSION \
+    --dart-define-from-file=.env \
+    && echo "See the apk in the $APK_FOLDER" \
+    && ls -lah $APK_FOLDER/*${VERSION}*.apk \
+    && echo "OK"
+fi
